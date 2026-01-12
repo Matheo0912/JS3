219cf671db75173f9cec7883bf1a8aee6ddc59be
Matheo0912 <matheo.onstad@gmail.com>
2026-01-12 09:24:50 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+// // oppg 1
+for (let i = 1; i <= 10; i++) {
+    console.log(`Hello world`);
+}
+
+// // oppg 2
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+//     // if (i % 2 !== 0) console.log("Hello World", i)
+// } 
+
+// // oppg 3
+// for (let i = 5; i <= 8; i++) {
+//     console.log(i);
+// }
+
+// // oppg 4
+// for (let i = 7; i >= 3; i--) {
+//     console.log(i);
+// }
+
+// // oppg 5
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 == 0) console.log(i)
+// }
+
+// // oppg 6
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 !== 0) console.log(i)
+// }
+
+// oppg 7
+// let summen = 0
+// for (let i = 1; i <= 4; i++) {
+//     summen += i;
+// }
+// console.log(summen)
+
+// // oppg 8
+let tekst = "";
+for (let i = 1; i <= 5; i++) {
+    tekst += "#"
+    console.log(tekst);
+}
