14bc76d11db840dde6ecab481097cb928fd3addb
Matheo0912 <matheo.onstad@gmail.com>
2026-01-12 09:26:26 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,37 +4,37 @@ for (let i = 1; i <= 10; i++) {
 }
 
 // // oppg 2
-// for (let i = 1; i <= 5; i++) {
-//     console.log(i);
-//     // if (i % 2 !== 0) console.log("Hello World", i)
-// } 
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+    // if (i % 2 !== 0) console.log("Hello World", i)
+} 
 
 // // oppg 3
-// for (let i = 5; i <= 8; i++) {
-//     console.log(i);
-// }
+for (let i = 5; i <= 8; i++) {
+    console.log(i);
+}
 
 // // oppg 4
-// for (let i = 7; i >= 3; i--) {
-//     console.log(i);
-// }
+for (let i = 7; i >= 3; i--) {
+    console.log(i);
+}
 
 // // oppg 5
-// for (let i = 2; i <= 10; i++) {
-//     if (i % 2 == 0) console.log(i)
-// }
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 == 0) console.log(i)
+}
 
 // // oppg 6
-// for (let i = 2; i <= 10; i++) {
-//     if (i % 2 !== 0) console.log(i)
-// }
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 !== 0) console.log(i)
+}
 
 // oppg 7
-// let summen = 0
-// for (let i = 1; i <= 4; i++) {
-//     summen += i;
-// }
-// console.log(summen)
+let summen = 0
+for (let i = 1; i <= 4; i++) {
+    summen += i;
+}
+console.log(summen)
 
 // // oppg 8
 let tekst = "";
